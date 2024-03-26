f01e69fd9bb771b28cd484734afd36c62c14fde2
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-03-26 08:57:41 -0500

    Start project

---
+++ src/main/java/pe/ironman/parallel/WebFluxParallelApplication.java
@@ -0,0 +1,13 @@
+package pe.ironman.parallel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxParallelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxParallelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/ironman/parallel/WebFluxParallelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ironman.parallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxParallelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
